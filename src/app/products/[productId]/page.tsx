82ac66d98db743eed5274ee58aa8982fc99dd813
hilmi_azman <muhammadhilmi92101@gmail.comm>
2023-12-23 19:37:42 +0800

    add Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,11 +1,37 @@
-const ProductDetail = ({
+import { Metadata } from 'next';
+
+type Props = {
+    params: {
+        productId: string
+    }
+}
+
+// export const generateMetadata = ({ params }: Props): Metadata => {
+//     return {
+//         title: `Product ${params.productId}`,
+//         description: `Product ${params.productId} is a very good item.`,
+//     };
+// };
+
+export const generateMetadata = async ({
     params,
-} : {
-    params: { productId: string }
-}) => {
+}: Props): Promise<Metadata> => {
+    const title = await new Promise((resolve) =>
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`);
+        }, 100)
+    );
+
+    return {
+        title: `Product ${title}`,
+    };
+};
+
+
+const ProductDetail = ({ params }: Props) => {
     return (
         <>
-            <h1>Product Detail { params.productId } </h1>
+            <h1>Product Detail {params.productId} </h1>
         </>
     );
 };
