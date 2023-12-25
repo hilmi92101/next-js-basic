69ffcf7ec13413389e757b907cf81824deb39e9f
hilmi_azman <muhammadhilmi92101@gmail.comm>
2023-12-26 01:12:39 +0800

    add Recovering from Errors

--- src/app/(landing)/error-handling/error.tsx
+++ src/app/(landing)/error-handling/error.tsx
@@ -1,11 +1,18 @@
 'use client'
 
-const ErrorBoundary = ({error}: { error: Error}) => {
+const ErrorBoundary = (
+    {error, reset}: 
+    { 
+        error: Error; 
+        reset: () => void;
+
+    }) => {
 
     return (
         <>
             <h1>Error message that come from error tsx file</h1>
             <h1>{error.message}</h1>
+            <button onClick={reset}>Try again</button>
 
         </>
     )
--- src/app/(landing)/error-handling/page.tsx
+++ src/app/(landing)/error-handling/page.tsx
@@ -1,14 +1,24 @@
+'use client'; // reason we use this to allow try again button works in error.tsx
+
+
+const getRandomInt = (count: number) => {
+
+    return Math.floor(Math.random() * count);
+}
+
+
 const ErrorHandling = () => {
 
-    const number = 1;
+    const number = getRandomInt(2);
+    console.log(number)
 
-    if(number === 1 ){
+    if(number === 1){
         //throw new Error('Error loading the page');
     }
 
     return (
         <>
-            <h1>Error Handling Page</h1>
+            <h1>Not an Error page</h1>
 
         </>
     )
