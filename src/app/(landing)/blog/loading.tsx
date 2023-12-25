2ef2312a8f0ef90ca2f6a8d2f81166c314275cd0
hilmi_azman <muhammadhilmi92101@gmail.comm>
2023-12-25 19:44:10 +0800

    add loading

---
+++ src/app/(landing)/blog/loading.tsx
@@ -0,0 +1,8 @@
+
+const Loading = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ab esse minus cumque possimus nulla nam illum, blanditiis eligendi tempora, debitis nisi temporibus veniam, minima aspernatur facilis totam cupiditate at. Maiores voluptatem assumenda ratione, ullam nam nostrum ea dolorum culpa fuga unde enim tenetur quisquam accusantium ad ipsam earum veritatis cupiditate rem nobis molestias iure delectus iusto? Hic inventore ab voluptates natus molestias aliquid eius sed molestiae assumenda, fuga voluptatibus aperiam. Ea deleniti deserunt eum! Quod consequatur molestias ea repellendus reprehenderit, laboriosam accusamus. Aut modi tempora pariatur officiis soluta error non debitis dolore inventore. Illo provident libero maiores quidem alias?</div>
+  )
+}
+
+export default Loading
