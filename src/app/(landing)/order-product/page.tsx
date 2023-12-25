276a46189fce71113e45040567aca949d180dd5a
hilmi_azman <muhammadhilmi92101@gmail.comm>
2023-12-25 17:46:44 +0800

    add Navigating Programmatically

---
+++ src/app/(landing)/order-product/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+
+export default function OrderProduct() {
+
+    const router = useRouter();
+
+    const handleClick = () => {
+
+        console.log('order placed')
+        router.push('/')
+        //router.replace('/')
+        //router.back()
+        //router.forward()
+    }
+
+    return (
+
+        <>
+            <h1>Order Product page</h1>
+            <button onClick={handleClick}>Place Order</button>
+        </>
+    )
+}
