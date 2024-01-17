a456cd7c1b3e1988682edb8c2e65fbc1da91a94e
hilmi_azman <muhammadhilmi92101@gmail.comm>
2024-01-17 18:14:54 +0800

    add reducer hook page

---
+++ src/app/react-hooks/use-reducer-hook/example-1/page.js
@@ -0,0 +1,17 @@
+"use client";
+
+import { useState, useEffect, createContext } from 'react'
+
+const Page = () => {
+
+
+    return (
+        <>
+            <h1>Example 1: useReducer Hook</h1>
+
+
+        </>
+    );
+};
+
+export default Page;
