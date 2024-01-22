10c72c3d9f85eec2fce7812a35127c734a36d0a9
hilmi_azman <hilmi.azman@newtycoon.com>
2024-01-22 22:24:14 +0800

    add example to react reducer hook

--- src/app/react-hooks/use-reducer-hook/example-1/page.js
+++ src/app/react-hooks/use-reducer-hook/example-1/page.js
@@ -1,14 +1,36 @@
 "use client";
 
-import { useState, useEffect, createContext } from 'react'
+import { useReducer } from 'react'
 
 const Page = () => {
+
+    const initialState = 0
+    const reducer = (state, action) => {
+        switch (action) {
+            case 'increment':
+                return state + 1
+            case 'decrement':
+                return state - 1
+            case 'reset':
+                return initialState
+            default:
+                return state
+        }
+    }
+
+    const [count, dispatch] = useReducer(reducer, initialState);
 
 
     return (
         <>
             <h1>Example 1: useReducer Hook</h1>
 
+            <div>
+                <div>Count = {count}</div>
+                <button onClick={() => dispatch('increment')}>Increment</button>
+                <button onClick={() => dispatch('decrement')}>Decrement</button>
+                <button onClick={() => dispatch('reset')}>Reset</button>
+            </div>
 
         </>
     );
