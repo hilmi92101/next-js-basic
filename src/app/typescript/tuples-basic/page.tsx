b8a32896ee69b1efa2342b6e22b4852253b2c7ea
hilmi <muhammadhilmi92101@gmail.com>
2024-01-29 22:58:41 +0800

    add few page for typescript example

---
+++ src/app/typescript/tuples-basic/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+
+let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+
+let student: [string, number];
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
+
+const page = () => {
+
+
+    return (
+
+        <>
+            <h1>Tuples</h1>
+
+
+        </>
+
+    )
+}
+
+export default page
